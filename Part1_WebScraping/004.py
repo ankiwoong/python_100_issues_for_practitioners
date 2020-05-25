1161a65b8d644c1c7e666c23c028857f11a994cb
ankiwoong <ankiwoong@gmail.com>
2020-05-25 13:54:26 +0900

    BeautifulSoup 객체 만들기

---
+++ Part1_WebScraping/004.py
@@ -0,0 +1,19 @@
+import requests
+from bs4 import BeautifulSoup
+
+url = "https://en.wikipedia.org/wiki/Seoul_Metropolitan_Subway"
+resp = requests.get(url)
+html_src = resp.text
+
+soup = BeautifulSoup(html_src, 'html.parser')
+print(type(soup))
+print('\n')
+
+print(soup.head)
+print('\n')
+print(soup.body)
+print('\n')
+
+print('title 태그 요소: ', soup.title)
+print('title 태그 이름: ', soup.title.name)
+print('title 태그 문자열: ', soup.title.string)
