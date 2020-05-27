8bacb8a556b39618228a86c7731d553fde9536c6
ankiwoong <ankiwoong@gmail.com>
2020-05-27 10:36:25 +0900

    전자공시시스템 API 활용하기 - DART 접속 및 XML 응답 객체 확인

---
+++ Part3_API/043.py
@@ -0,0 +1,14 @@
+import requests
+from bs4 import BeautifulSoup
+
+# DART 전자공시 사이트 APT 인증키 입력
+crtfc_key = "d691b6402660bb64db89e7e06ef636ccdf879e01"
+
+# 기업개황 정보 접속 URL
+corp_code = "00126380"
+url = 'https://opendart.fss.or.kr/api/company.xml?crtfc_key=' + \
+    crtfc_key + '&corp_code=' + corp_code
+# BeautifulSoup으로 API가 반환하는 XML 확인
+xml = requests.get(url)
+soup = BeautifulSoup(xml.text, 'html.parser')
+print(soup)
